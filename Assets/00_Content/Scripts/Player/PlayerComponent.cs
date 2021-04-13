c085794cdc8fefaa0fcf5a44f7aa7cec6b0fe416
Jonatan Johansson <smeas@users.noreply.github.com>
2021-04-13 14:48:26 +0200

    Add player scripts
    - Basic PlayerComponent
    - PlayerInputHandler
    - Simple camera relative PlayerMovement

---
+++ Assets/00_Content/Scripts/Player/PlayerComponent.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player {
+	public class PlayerComponent : MonoBehaviour {
+		public int PlayerId { get; private set; }
+
+		private void Awake() {
+			PlayerInput playerInput = GetComponent<PlayerInput>();
+			PlayerId = playerInput.playerIndex;
+		}
+	}
+}
