026b569dd4eb03119fe948d932f17dd48ad85aaa
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-14 15:02:44 +0200

    Create manager to help swapping scenes

---
+++ Assets/00_Content/Scripts/Scenes/SceneLoadManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+namespace Scenes {
+	public class SceneLoadManager : SingletonBehaviour<SceneLoadManager> {
+		[SerializeField] private SceneInfo mainMenu;
+		[SerializeField] private SceneInfo game;
+
+		public SceneInfo CurrentScene {
+			get {
+				int currentIndex = SceneManager.GetActiveScene().buildIndex;
+				if (currentIndex == mainMenu.scene.BuildIndex)
+					return mainMenu;
+				if (currentIndex == game.scene.BuildIndex)
+					return game;
+
+				return null;
+			}
+		}
+
+		protected override void Awake() {
+			base.Awake();
+
+			DontDestroyOnLoad(gameObject);
+		}
+
+		public void LoadMainMenu() {
+			mainMenu.Load();
+		}
+
+		public void LoadGame() {
+			game.Load();
+		}
+	}
+}
