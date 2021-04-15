8ce62f8eee36b0048beb057a48015651853a6e66
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-15 15:09:28 +0200

    Update normal viking data

--- Assets/00_Content/Scripts/ScriptableObjects/VikingData.cs
+++ Assets/00_Content/Scripts/ScriptableObjects/VikingData.cs
@@ -3,9 +3,10 @@
 namespace ScriptableObjects {
 	[CreateAssetMenu(fileName = "new Viking", menuName = "VikingData", order = 0)]
 	public class VikingData : ScriptableObject {
+		[Range(0, 100)]
+		public float startMood;
+
 		[Tooltip("Mood/s")]
 		public float moodDeclineRate;
-
-		public float startMood;
 	}
 }
