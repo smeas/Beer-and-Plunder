f5e4e34674e9f2cd26d6d5ec9568561916a0d3dc
Jonatan Johansson <smeas@users.noreply.github.com>
2021-04-15 12:25:54 +0200

    Workaround bug in InputSystem that invoked events on player prefab
    (cherry picked from commit 295761dd2d11552e76608543921b6b707fca8106)

--- Assets/00_Content/Scripts/Player/PlayerInputHandler.cs
+++ Assets/00_Content/Scripts/Player/PlayerInputHandler.cs
@@ -18,10 +18,24 @@ namespace Player {
 		public UnityEvent OnDrop => onDrop;
 		public UnityEvent OnInteract => onInteract;
 
+		private PlayerInput playerInput;
+
+		// Workaround for a bug in the in the input system where events are executed on the prefab.
+		private bool ShouldExecuteEvents {
+			get {
+				if (playerInput == null)
+					playerInput = GetComponent<PlayerInput>();
+
+				// Check if we are a real player
+				return playerInput.playerIndex != -1;
+			}
+		}
+
 		// These methods are invoked by the UnityEvents on the PlayerInput component.
 		#region Input Action Handlers
 
 		public void OnMoveInput(InputAction.CallbackContext ctx) {
+			if (!ShouldExecuteEvents) return;
 			if (ctx.performed)
 				onMove.Invoke(ctx.ReadValue<Vector2>());
 			else if (ctx.canceled)
@@ -29,6 +43,7 @@ namespace Player {
 		}
 
 		public void OnUseInput(InputAction.CallbackContext ctx) {
+			if (!ShouldExecuteEvents) return;
 			if (ctx.performed)
 				onUse.Invoke();
 			else if (ctx.canceled)
@@ -36,16 +51,19 @@ namespace Player {
 		}
 
 		public void OnPickupInput(InputAction.CallbackContext ctx) {
+			if (!ShouldExecuteEvents) return;
 			if (ctx.performed)
 				onPickup.Invoke();
 		}
 
 		public void OnDropInput(InputAction.CallbackContext ctx) {
+			if (!ShouldExecuteEvents) return;
 			if (ctx.performed)
 				onDrop.Invoke();
 		}
 
 		public void OnInteractInput(InputAction.CallbackContext ctx) {
+			if (!ShouldExecuteEvents) return;
 			if (ctx.performed)
 				onInteract.Invoke();
 		}
