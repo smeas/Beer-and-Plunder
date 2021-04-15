7a1d6fed31eb8de63d4881ab757732a375f82fc6
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-15 15:06:24 +0200

    Create objects controlling difficulty scaling

---
+++ Assets/00_Content/Scripts/ScriptableObjects/ScalingData.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects {
+	[CreateAssetMenu(fileName = "new ScalingData", menuName = "ScalingData", order = 0)]
+	public class ScalingData : ScriptableObject {
+		[field: SerializeField] private float InitialSpawnDelay { get; set; } = 1f;
+
+		[field: SerializeField, Tooltip("Additive change/round")]
+		private float SpawnDelayMultiplier { get; set; } = 1f;
+
+		[field: SerializeField, Tooltip("Additive change/round")]
+		private float StartingMoodMultiplier { get; set; } = 1f;
+
+		[field: SerializeField, Tooltip("Additive change/round")]
+		private float MoodDeclineMultiplier { get; set; } = 1f;
+
+		// These calculations will need to be modified later to balance the game
+	#region ScalingCalculations
+
+		/// <summary>
+		/// Scales the spawn delay linearly in relation to <paramref name="round"/>
+		/// </summary>
+		public float ScaledSpawnDelay(int round) {
+			return InitialSpawnDelay * CalcLinear(-SpawnDelayMultiplier + 1, round, 1);
+		}
+
+		/// <summary>
+		/// Scales the starting mood linearly in relation to <paramref name="round"/>
+		/// </summary>
+		public float ScaledStartingMood(int round) {
+			return CalcLinear(StartingMoodMultiplier - 1, round, 1);
+		}
+
+		/// <summary>
+		/// Scales the mood decline multiplier linearly in relation to <paramref name="round"/>
+		/// </summary>
+		public float ScaledMoodDeclineMultiplier(int round) {
+			return CalcLinear(MoodDeclineMultiplier - 1, round, 1);
+		}
+
+		/// <summary>
+		/// Calculates a value following f(x) = kx + m
+		/// </summary>
+		public static float CalcLinear(float k, int x, float m) {
+			return k * x + m;
+		}
+
+		#endregion
+
+	}
+}
