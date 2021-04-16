fc95de0fb29a42c7ed9b46974232a9211bf3a2a1
Jonatan Johansson <smeas@users.noreply.github.com>
2021-04-16 13:31:50 +0200

    Add table and chair

---
+++ Assets/00_Content/Scripts/Interactables/Table.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Vikings;
+
+namespace Interactables {
+	public class Table : Interactable {
+		[SerializeField] private Chair[] chairs;
+
+		public bool TryFindEmptyChairForViking(Viking viking, out Chair closest) {
+			Vector3 vikingPosition = viking.transform.position;
+
+			closest = null;
+			float minDistance = float.PositiveInfinity;
+
+			foreach (Chair chair in chairs) {
+				if (chair.SittingViking != null)
+					continue;
+
+				float distance = (vikingPosition - chair.SitPivot.position).sqrMagnitude;
+				if (distance < minDistance) {
+					minDistance = distance;
+					closest = chair;
+				}
+			}
+
+			return closest != null;
+		}
+	}
+}
