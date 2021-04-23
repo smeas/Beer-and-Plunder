5f8daf694f02519911bd6b599582b8ce93e5c6cd
DreamingJohnny <johnnydreaming@gmail.com>
2021-04-23 11:20:30 +0200

    Basic MainMenu that takes you to the Lobby

--- Assets/00_Content/Scripts/Menu/MenuController.cs
+++ Assets/00_Content/Scripts/Menu/MenuController.cs
@@ -1,17 +1,35 @@
 using Scenes;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace Menu {
 	public class MenuController : MonoBehaviour {
 
+		public GameObject mainMenuPanel;
+		public GameObject settingsPanel;
 		//So, this will later on have some way of turning on and of what input to be using yeah?
 
+		private void Start() {
+			settingsPanel.SetActive(false);
+			mainMenuPanel.SetActive(true);
+		}
+
 		public void GoToLobby() {
+			mainMenuPanel.SetActive(false);
+			settingsPanel.SetActive(false);
+
 			SceneLoadManager.Instance.LoadLobby();
 		}
 
 		public void GoToSettings() {
-			//this one can activate another panel for settings on top of it.
+			mainMenuPanel.SetActive(false);
+			settingsPanel.SetActive(true);
+		}
+
+		public void ReturnToMainMenu() {
+			settingsPanel.SetActive(false);
+			mainMenuPanel.SetActive(true);
 		}
 
 		public void QuitGame() {
