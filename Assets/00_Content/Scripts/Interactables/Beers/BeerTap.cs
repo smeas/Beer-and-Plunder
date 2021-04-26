73afb9b20b7235a07ea7c297745274327f5ff840
Jonatan Johansson <smeas@users.noreply.github.com>
2021-04-26 21:28:47 +0200

    Remove redundant beerSpawnpoint from BeerTap

--- Assets/00_Content/Scripts/Interactables/Beers/BeerTap.cs
+++ Assets/00_Content/Scripts/Interactables/Beers/BeerTap.cs
@@ -12,7 +12,6 @@ namespace Interactables.Beers {
 
 		[Header("GameObjects")]
 		[SerializeField] private GameObject beerPrefab;
-		[SerializeField] private Transform beerSpawnpoint;
 		[SerializeField] private Image progressBarImage;
 		[SerializeField] private GameObject progressBar;
 
@@ -22,9 +21,6 @@ namespace Interactables.Beers {
 
 		private void Start() {
 			itemSlot = GetComponentInChildren<ItemSlot>();
-
-			if (beerSpawnpoint == null)
-				Debug.LogError("No spawnpoint for beer on beerTap");
 		}
 
 		public override void Interact(GameObject player, PickUp item) {
@@ -51,7 +47,7 @@ namespace Interactables.Beers {
 				progressBarImage.fillAmount = pouringProgress * 0.01f;
 
 				if(pouringProgress > 100) {
-					GameObject beer = Instantiate(beerPrefab, beerSpawnpoint.position, Quaternion.identity);
+					GameObject beer = Instantiate(beerPrefab);
 					itemSlot.PutItem(beer.GetComponent<PickUp>());
 
 					pouringProgress = 0;
