ad46f980f064f88789dfe801e0bc84b654555d04
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-27 13:59:36 +0200

    Create utility math class

---
+++ Assets/00_Content/Scripts/Utilities/MathX.cs
@@ -0,0 +1,7 @@
+﻿namespace Utilities {
+	public static class MathX {
+		public static float Remap(float value, float low1, float high1, float low2, float high2) {
+			return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+		}
+	}
+}
