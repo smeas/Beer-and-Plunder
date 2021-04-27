56aa9d8f6ae935800af1a0c8d31d1de4c0835085
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-27 10:07:43 +0200

    Create desirevisualiser

---
+++ Assets/00_Content/Scripts/Vikings/DesireVisualiser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Vikings {
+	public class DesireVisualiser : MonoBehaviour {
+		private GameObject visualisation;
+
+		public void ShowNewDesire(GameObject desireVisualisationPrefab) {
+			if (visualisation == null) Destroy(visualisation);
+
+			if (desireVisualisationPrefab == null) {
+				Debug.Assert(false, "Desire visualisation is null");
+				return;
+			}
+
+			visualisation = Instantiate(desireVisualisationPrefab, transform);
+		}
+
+		public void HideDesire() {
+			if (visualisation == null) {
+				Debug.Assert(false, "Trying to remove a non-existing desire visualisation");
+				return;
+			}
+
+			Destroy(visualisation);
+		}
+	}
+}
