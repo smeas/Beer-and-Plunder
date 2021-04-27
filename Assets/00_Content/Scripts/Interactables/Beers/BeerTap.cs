2cf5d9238ff48a39d3960b717989476b6433a545
Jonatan Johansson <smeas@users.noreply.github.com>
2021-04-27 10:45:29 +0200

    Make BeerTap not require a Tavern

--- Assets/00_Content/Scripts/Interactables/Beers/BeerTap.cs
+++ Assets/00_Content/Scripts/Interactables/Beers/BeerTap.cs
@@ -1,7 +1,7 @@
 using System.Collections;
+using Taverns;
 using UnityEngine;
 using UnityEngine.UI;
-using Taverns;
 
 namespace Interactables.Beers {
 
@@ -15,7 +15,6 @@ namespace Interactables.Beers {
 		[SerializeField] private GameObject beerPrefab;
 		[SerializeField] private Image progressBarImage;
 		[SerializeField] private GameObject progressBar;
-		//I added a field here for the beerData SO, so that I could get the cost from it.
 		[SerializeField] private BeerData beerData;
 
 		private ItemSlot itemSlot;
@@ -41,37 +40,34 @@ namespace Interactables.Beers {
 		}
 
 		private IEnumerator PourBeer() {
+			if (Tavern.Instance != null && Tavern.Instance.Money < beerData.cost)
+				yield break;
 
-			if (Tavern.Instance.Money >= beerData.cost) {
-				//I added that the tavern singleton must contain more money than the cost of the beer in order for the player to be able to pour the beer at all.
-				while (!itemSlot.HasItemInSlot && isHolding && pouringProgress <= 100 && Tavern.Instance.Money >= beerData.cost) {
+			while (!itemSlot.HasItemInSlot && isHolding && pouringProgress <= 100) {
+				if (Tavern.Instance != null && Tavern.Instance.Money < beerData.cost)
+					break;
 
-					pouringProgress += pourTimeMultiplier * Time.deltaTime;
+				pouringProgress += pourTimeMultiplier * Time.deltaTime;
 
-					if (!progressBar.activeInHierarchy)
-						progressBar.SetActive(true);
+				if (!progressBar.activeInHierarchy)
+					progressBar.SetActive(true);
 
-					progressBarImage.fillAmount = pouringProgress * 0.01f;
+				progressBarImage.fillAmount = pouringProgress * 0.01f;
 
-					if (pouringProgress > 100) {
-						GameObject beer = Instantiate(beerPrefab);
-						itemSlot.PutItem(beer.GetComponent<PickUp>());
+				if (pouringProgress > 100) {
+					GameObject beer = Instantiate(beerPrefab);
+					itemSlot.PutItem(beer.GetComponent<PickUp>());
 
-						//Added a line of code so that the player draws the cost just as the beer is done.
+					if (Tavern.Instance != null)
 						Tavern.Instance.SpendsMoney(beerData.cost);
 
-						pouringProgress = 0;
-						progressBar.SetActive(false);
-						break;
-
-					}
-
-					yield return null;
+					pouringProgress = 0;
+					progressBar.SetActive(false);
+					break;
 				}
-			} else {
-				Debug.Log("You don't have enough money to pour a beer!");
-			}
 
+				yield return null;
+			}
 		}
 	}
 }
