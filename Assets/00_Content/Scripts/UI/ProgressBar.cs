87395ac255a9c40ee0ddf905689991ae1d009967
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-04-28 11:16:41 +0200

    Streamline progressbar via progressbar class

---
+++ Assets/00_Content/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+	public class ProgressBar : MonoBehaviour {
+		[SerializeField] private Image progressImage;
+
+		private void OnEnable() {
+			transform.forward = Vector3.forward;
+		}
+
+		public void Show() {
+			gameObject.SetActive(true);
+		}
+
+		public void Hide() {
+			gameObject.SetActive(false);
+		}
+
+		public void UpdateProgress(float progress) {
+			progressImage.fillAmount = progress;
+		}
+	}
+}
