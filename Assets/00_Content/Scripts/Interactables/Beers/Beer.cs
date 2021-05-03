0decb8e74ee1b48db20c4ca75da1fd3369b61bae
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-05-03 12:08:05 +0200

    Make beer not IUsable

--- Assets/00_Content/Scripts/Interactables/Beers/Beer.cs
+++ Assets/00_Content/Scripts/Interactables/Beers/Beer.cs
@@ -2,21 +2,10 @@ using UnityEngine;
 using Vikings;
 
 namespace Interactables.Beers {
-	public class Beer : PickUp, IUseable, IDesirable {
+	public class Beer : PickUp, IDesirable {
 
 		[SerializeField] private BeerData beerData;
 
 		public DesireType DesireType => beerData.type;
-
-		public void Use(GameObject user) {
-			//Drink it? Temporary fighting buff lulz xD
-
-			//Give to customer
-
-			Debug.Log("Using item beer...");
-		}
-
-		public void EndUse() { }
-
 	}
 }
