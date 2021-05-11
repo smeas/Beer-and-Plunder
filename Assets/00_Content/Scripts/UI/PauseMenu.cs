b33e7eb40669bab002eb1f35131bd7c9f4cf0104
Jonatan Johansson <smeas@users.noreply.github.com>
2021-05-11 12:45:28 +0200

    Add basic pause menu

---
+++ Assets/00_Content/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,64 @@
+using Scenes;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using Utilities;
+
+namespace UI {
+	public class PauseMenu : SingletonBehaviour<PauseMenu> {
+		[SerializeField] private InputActionProperty pauseAction;
+		[SerializeField] private Selectable firstSelected;
+
+		private Canvas canvas;
+		private bool isPaused;
+
+		public bool IsPaused => isPaused;
+
+		protected override void Awake() {
+			base.Awake();
+
+			canvas = GetComponent<Canvas>();
+			canvas.enabled = false;
+			pauseAction.action.Enable();
+			pauseAction.action.performed += HandleOnPausePressed;
+		}
+
+		protected override void OnDestroy() {
+			base.OnDestroy();
+
+			pauseAction.action.performed -= HandleOnPausePressed;
+		}
+
+		private void HandleOnPausePressed(InputAction.CallbackContext ctx) {
+			TogglePaused();
+		}
+
+		public void TogglePaused() {
+			isPaused = !isPaused;
+			if (isPaused) {
+				canvas.enabled = true;
+				EventSystem.current.SetSelectedGameObject(firstSelected.gameObject);
+				Time.timeScale = 0f;
+			}
+			else {
+				canvas.enabled = false;
+				Time.timeScale = 1f;
+			}
+		}
+
+		// Unity event functions
+
+		public void Resume() {
+			TogglePaused();
+		}
+
+		public void Options() { /* TODO */ }
+
+		public void ExitToMenu() {
+			// FIXME: This resumes the game while doing the scene transition which might cause problems.
+			Time.timeScale = 1f;
+			SceneLoadManager.Instance.LoadMainMenu();
+		}
+	}
+}
