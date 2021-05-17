100fd47ac9f11b205398af0a19d4f27319e2dd95
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-05-17 15:54:49 +0200

    Modify menucontroller to allow for easier modifications

--- Assets/00_Content/Scripts/Menu/MenuController.cs
+++ Assets/00_Content/Scripts/Menu/MenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Scenes;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,25 +9,29 @@ using UnityEngine.Timeline;
 
 namespace Menu {
 	public class MenuController : MonoBehaviour {
-		[SerializeField] private GameObject mainMenuPanel;
-		[SerializeField] private GameObject settingsPanel;
+		[SerializeField] private GameObject startPanel;
+		[SerializeField] private GameObject backButton;
+
+		[Space]
 		[SerializeField] private GameObject startGameButton;
 
 		[Header("Timeline")]
 		[SerializeField] private PlayableDirector timelineDirector;
 		[SerializeField] private TimelineAsset toLobbyTimeline;
 
+		private Stack<GameObject> panels = new Stack<GameObject>();
+
 		private void Start() {
-
 			if (PlayerManager.Instance != null) {
-
 				foreach (PlayerComponent player in PlayerManager.Instance.Players) {
 					PlayerInput playerInput = player.GetComponent<PlayerInput>();
 					playerInput.SwitchCurrentActionMap("UI");
 				}
 			}
-			settingsPanel.SetActive(false);
-			mainMenuPanel.SetActive(true);
+
+			startPanel.SetActive(true);
+			panels.Push(startPanel);
+			backButton.SetActive(false);
 		}
 
 		public void GoToLobby() {
@@ -39,14 +44,21 @@ namespace Menu {
 			SceneLoadManager.Instance.LoadTutorial();
 		}
 
-		public void GoToSettings() {
-			mainMenuPanel.SetActive(false);
-			settingsPanel.SetActive(true);
+		public void ShowPanel(GameObject panel) {
+			panels.Peek().SetActive(false);
+			panel.SetActive(true);
+			panels.Push(panel);
+
+			if (panels.Count > 1)
+				backButton.SetActive(true);
 		}
 
-		public void ReturnToMainMenu() {
-			settingsPanel.SetActive(false);
-			mainMenuPanel.SetActive(true);
+		public void ClosePanel() {
+			panels.Pop().SetActive(false);
+			panels.Peek().SetActive(true);
+
+			if (panels.Count == 1)
+				backButton.SetActive(false);
 		}
 
 		public void QuitGame() {
